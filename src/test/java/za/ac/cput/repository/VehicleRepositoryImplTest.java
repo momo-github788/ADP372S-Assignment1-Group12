1a22312010c6ec783d16b256e214e5b4b45f6edf
momo-github788 <momoZzzz1996@gmail.com>
2023-04-07 17:58:41 +0200

    Added Location And Vehicle Repository Interfaces, Implementation classes and Test Classes

---
+++ src/test/java/za/ac/cput/repository/VehicleRepositoryImplTest.java
@@ -0,0 +1,73 @@
+package za.ac.cput.repository;
+
+/*  VehicleRepositoryImplTest.java
+    Test class for VehicleRepositoryImpl
+    Author: Muhammed Luqmaan Hoosain (220464901)
+    Date: 7 April 2023
+*/
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import za.ac.cput.domain.FuelType;
+import za.ac.cput.domain.Vehicle;
+import za.ac.cput.domain.VehicleCondition;
+import za.ac.cput.factory.VehicleFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class VehicleRepositoryImplTest {
+
+    private static VehicleRepositoryImpl vehicleRepository = VehicleRepositoryImpl.getVehicleRepository();
+    private static Vehicle vehicle = VehicleFactory.createVehicle("Audi", "A4", VehicleCondition.USED, FuelType.PETROL, "White", 2019, 23000);
+
+
+    @Order(1)
+    @Test
+    void create() {
+        Vehicle createdVehicle = vehicleRepository.create(vehicle);
+        assertNotNull(createdVehicle);
+        System.out.println("Create: " + createdVehicle);
+    }
+
+    @Order(2)
+    @Test
+    void read() {
+        Vehicle readVehicle = vehicleRepository.read(vehicle.getVehicleId());
+        assertNotNull(readVehicle);
+        System.out.println("Read: " + readVehicle);
+    }
+
+    @Order(3)
+    @Test
+    void update() {
+        Vehicle updatedVehicle = new Vehicle.VehicleBuilder()
+                .copy(vehicle)
+                .setMileage(59999)
+                .build();
+        assertNotNull(vehicleRepository.update(updatedVehicle));
+        System.out.println("Update: " + updatedVehicle);
+
+
+    }
+
+    @Order(5)
+    @Test
+    void delete() {
+        boolean success = vehicleRepository.delete(vehicle.getVehicleId());
+
+        assertTrue(success);
+        System.out.println("Delete: " + success);
+    }
+
+    @Order(4)
+    @Test
+    void getAll() {
+        System.out.println("Get all: ");
+        System.out.println(vehicleRepository.getAll());
+        assertEquals(1, vehicleRepository.getAll().size());
+    }
+
+}
