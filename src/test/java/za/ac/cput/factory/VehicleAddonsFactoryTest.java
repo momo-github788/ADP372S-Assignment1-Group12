830348a8e0af354e8b9e1c1acb900776534af476
SERGE KALALA <220525137@mycput.ac.za>
2023-04-09 16:03:51 +0200

    I finish my change

--- src/test/java/za/ac/cput/factory/VehicleAddonsFactoryTest.java
+++ src/test/java/za/ac/cput/factory/VehicleAddonsFactoryTest.java
@@ -2,7 +2,7 @@ package za.ac.cput.factory;
 /* VehicleAddonsFactoryTest.java
     Test class for VehicleAddons
     Author: Kimoki Serge Kalala (220525137)
-    Date: 07 April 2023
+    Date: 07/04/2023
 */
 
 
@@ -18,7 +18,7 @@ class VehicleAddonsFactoryTest {
 
     @Test
     void createVehicleAddonsFactory() {
-        vehicleAddons  =VehicleAddonsFactory.createVehicleAddonsFactory("289273","8736736");
+        vehicleAddons  =VehicleAddonsFactory.createVehicleAddonsFactory("45677","97654");
           assertNotNull(vehicleAddons);
     }
 
