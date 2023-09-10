5dd0851d0aae718f6ff690e85eb34e88154de7f2
momo-github788 <momoZzzz1996@gmail.com>
2023-09-10 23:13:36 +0200

    final

--- src/main/java/za/ac/cput/vehicledealership/controller/thymeleaf/SignUpController.java
+++ src/main/java/za/ac/cput/vehicledealership/controller/thymeleaf/SignUpController.java
@@ -1,21 +1,33 @@
 package za.ac.cput.vehicledealership.controller.thymeleaf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import za.ac.cput.vehicledealership.domain.User;
+import za.ac.cput.vehicledealership.service.impl.UserServiceImpl;
 
 @Controller
 public class SignUpController {
-    //method to get signup form
-    @RequestMapping(value="/signup", method= RequestMethod.GET)
-    public String getUser(@ModelAttribute User user) {
-        //Testing purposes
-        System.out.println("GET Testing");
+
+    private UserServiceImpl userService;
+
+    @Autowired
+    public SignUpController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(value="/signup")
+    public String submitSignUpUserForm(@ModelAttribute User user) {
+        userService.saveUser(user);
         return "signup-form";
     }
+
     //method to get signupForm details
-    @RequestMapping(value = "/signup", method= RequestMethod.POST)
-    public String user(@ModelAttribute User user) {
-        return "login-form";
+    @GetMapping(value = "/signup")
+    public String showSignUpForm(Model model) {
+        model.addAttribute("user", new User());
+        return "signup-form";
     }
+
 }
