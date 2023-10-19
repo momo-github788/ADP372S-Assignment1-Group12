3e0381625a5999e046a529139e5950b6fc1c944a
momo-github788 <momoZzzz1996@gmail.com>
2023-10-19 06:20:06 +0200

    reference env variable for password

--- src/test/java/za/ac/cput/vehicledealership/factory/InventoryFactoryTest.java
+++ src/test/java/za/ac/cput/vehicledealership/factory/InventoryFactoryTest.java
@@ -1,28 +1,30 @@
-//package za.ac.cput.vehicledealership.factory;
-///* InventoryFactoryTest.java
-//    Test class for Inventory
-//    Author: Kimoki Serge Kalala (220525137)
-//    Date: 07 April 2023
-//*/
-//
-//import org.junit.jupiter.api.Disabled;
-//import org.junit.jupiter.api.Test;
-//import za.ac.cput.vehicledealership.domain.*;
-//
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//import static org.junit.jupiter.api.Assertions.assertNull;
-//
-//class InventoryFactoryTest {
-//
-//    @Test
-//    void createInventoryFactoryaSuccess() {
-//        Vehicle vehicle = VehicleFactory.createVehicle("Audi", "A4", VehicleCondition.USED, FuelType.PETROL, BodyType.SEDAN, "White", 2019, 23000);
-//
-//        Inventory inventory= InventoryFactory.createInventoryFactory(43, InventoryType.USED,vehicle);
-//        System.out.println(inventory);
-//        assertNotNull(inventory);
-//
-//
-//    }
-//
-//}
+package za.ac.cput.vehicledealership.factory;
+/* InventoryFactoryTest.java
+    Test class for Inventory
+    Author: Kimoki Serge Kalala (220525137)
+    Date: 07 April 2023
+*/
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.vehicledealership.domain.*;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class InventoryFactoryTest {
+
+    @Test
+    void createInventoryFactoryaSuccess() {
+        Location location = LocationFactory.createLocation(89974, "Main Road", "Cape Town", 7626, "Western Cape");
+
+        Branch branch = BranchFactory.createBranch("Paarl Auto",2008,location);
+
+        Inventory inventory= InventoryFactory.createInventoryFactory("My Inventory", 2, InventoryType.USED,branch);
+        System.out.println(inventory);
+        assertNotNull(inventory);
+
+
+    }
+
+}
